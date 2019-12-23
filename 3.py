0d90e404f538bb34ff5991a851a25448216f0527
spartio <simopartio@gmail.com>
2019-12-23 10:04:35 +0200

    valmis

--- 3.py
+++ 3.py
@@ -1,12 +1,3 @@
-'''
-s1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-s1 = s1.split(',')
-s2 = "U62,R66,U55,R34,D71,R55,D58,R83"
-s2 = s2.split(',')
-'''
-
-
-
 def calc(path):
     curx = cury = step = 0
     directions = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
@@ -21,11 +12,15 @@ def calc(path):
                 points[(curx, cury)] = step
     return points
 
+lines = open("input3").read().splitlines()
+s1 = lines[0].split(',')
+s2 = lines[1].split(',')
+
 
 s1_points = calc(s1)
 s2_points = calc(s2)
 intersection_points = [point for point in s1_points if point in s2_points]
 
 part1 = min(abs(x) + abs(y) for (x, y) in intersection_points)
-
-print(part1)
+part2 = min(s1_points[point] + s2_points[point] for point in intersection_points)
+print('Part 1: {0}, Part 2: {1}'.format(part1, part2))
